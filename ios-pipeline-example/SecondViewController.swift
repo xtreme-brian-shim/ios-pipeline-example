42a825da957d15740dde62c65c944e4b48e33a13
Brian Shim <bshim@pivotal.io>
2020-03-24 10:55:03 -0400

    Initial iOS project commit

---
+++ ios-pipeline-example/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ios-pipeline-example
+//
+//  Created by Brian Shim on 3/24/20.
+//  Copyright © 2020 Brian Shim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
